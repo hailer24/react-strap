b0846f8bd0588cff11c88c5b26bc3e797f2620cd
hailer24 <40500174+hailer24@users.noreply.github.com>
2021-11-14 12:13:38 +0530

    Modified main component

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -43,7 +43,7 @@ class DishDetail extends Component {
             -- {comment.author}, &nbsp;
             {new Intl.DateTimeFormat("en-US", {
               year: "numeric",
-              month: "long",
+              month: "short",
               day: "2-digit",
             }).format(new Date(comment.date))}
           </p>
