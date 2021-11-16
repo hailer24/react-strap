6dc84875e39d94ffa1fa2c3f47d5f6c1ef41dccc
hailer24 <40500174+hailer24@users.noreply.github.com>
2021-11-16 16:47:12 +0530

    added login and redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,11 +3,19 @@ import { NavLink } from "react-router-dom";
 import {
   Navbar,
   NavbarBrand,
-  Jumbotron,
   Nav,
-  NavItem,
-  Collapse,
   NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Input,
+  Label,
 } from "reactstrap";
 
 export default class Header extends Component {
@@ -15,9 +23,31 @@ export default class Header extends Component {
     super(props);
 
     this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+
     this.state = {
       isNavOpen: false,
+      isModalOpen: false,
     };
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    alert(
+      "Username: " +
+        this.username.value +
+        " Password: " +
+        this.password.value +
+        " Remember: " +
+        this.remember.checked
+    );
+    event.preventDefault();
+  }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
   }
 
   toggleNav() {
@@ -64,6 +94,13 @@ export default class Header extends Component {
                   </NavLink>
                 </NavItem>
               </Nav>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span> Login
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
           </div>
         </Navbar>
@@ -81,6 +118,44 @@ export default class Header extends Component {
             </div>
           </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  innerRef={(input) => (this.username = input)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  innerRef={(input) => (this.password = input)}
+                />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    name="remember"
+                    innerRef={(input) => (this.remember = input)}
+                  />
+                  Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
       </div>
     );
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,30 +1,24 @@
 import React from "react";
 import Menu from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
-import { COMMENTS } from "../shared/comments";
-import { PROMOTIONS } from "../shared/promotions";
-import { LEADERS } from "../shared/leaders";
 import DishDetail from "./DishDetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
-import { Switch, Route, Redirect } from "react-router-dom";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders,
+  };
+};
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS,
-      selectedDish: null,
-    };
-  }
-
   onDishSelect(dish) {
     this.setState({ selectedDish: dish });
   }
@@ -36,11 +30,11 @@ class Main extends React.Component {
       return (
         <DishDetail
           dish={
-            this.state.dishes.filter(
+            this.props.dishes.filter(
               (dish) => dish.id === parseInt(match.params.dishId, 10)
             )[0]
           }
-          comments={this.state.comments.filter(
+          comments={this.props.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
         />
@@ -49,14 +43,14 @@ class Main extends React.Component {
     const HomePage = () => {
       return (
         <Home
-          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-          promotion={this.state.promotions.filter((e) => e.featured)[0]}
-          leader={this.state.leaders.filter((e) => e.featured)[0]}
+          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.props.promotions.filter((e) => e.featured)[0]}
+          leader={this.props.leaders.filter((e) => e.featured)[0]}
         />
       );
     };
     const Aboutus = () => {
-      return <About leaders={this.state.leaders} />;
+      return <About leaders={this.props.leaders} />;
     };
     return (
       <div>
@@ -68,7 +62,7 @@ class Main extends React.Component {
             path="/menu"
             component={() => (
               <>
-                <Menu dishes={this.state.dishes} />
+                <Menu dishes={this.props.dishes} />
                 <DishDetail />
               </>
             )}
@@ -84,4 +78,4 @@ class Main extends React.Component {
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
